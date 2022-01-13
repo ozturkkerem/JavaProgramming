692a352367e78669642f16d6634142e4c770b92d
ozturkkerem <ozturk.kerem@live.com>
2022-01-13 12:15:16 -0500

    Inheritance exerxzcises

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,14 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", 2021,20000,"Gray",122000);
+
+        Tesla tesla = new Tesla("Model S", 2022,95000,"Red",0);
+
+        BMW bmw = new BMW("x5",2010,20000,"Black",1000);
+        //
+
+    }
+}
