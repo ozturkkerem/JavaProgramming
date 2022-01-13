d51917ba6a57727029b6cdff8082ecd5422b44bd
ozturkkerem <ozturk.kerem@live.com>
2022-01-13 11:03:50 -0500

    Inheritance exercises

--- src/day38_Inheritance/carTask/Tesla.java
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -3,7 +3,13 @@ package day38_Inheritance.carTask;
 public class Tesla extends Car {
 
 
-    public Tesla(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public Tesla( String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
     }
+
+    public void autoPilot(){
+
+        System.out.println(brand+" "+ " in autopilot mode");
+    }
+
 }
