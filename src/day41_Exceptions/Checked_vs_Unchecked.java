be91e433483a1da0d4322083c924a86637fb2474
ozturkkerem <ozturk.kerem@live.com>
2022-01-21 10:39:59 -0500

    first

---
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -0,0 +1,25 @@
+package day41_Exceptions;
+
+public class Checked_vs_Unchecked {
+
+    public static void main(String[] args) {
+
+        //unchecked exception:
+
+        int a = 10;
+        int b = 0;
+
+       // System.out.println(a/b);
+
+
+     //   System.out.println("Wooden Spoon");
+
+            char[] characters = {'A', 'B', 'C'};
+                                //0    1    2
+
+        System.out.println(characters[99]);
+
+
+    }
+
+}
